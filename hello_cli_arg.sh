b81650cbccf6fb0e072e8493f19ecf14bf69f11f
balagesh <balagesh.h@gmail.com>
2024-12-12 10:08:53 +0000

    szöveges cli arg próbája

--- hello_cli_arg.sh
+++ hello_cli_arg.sh
@@ -1,2 +1,19 @@
+# Exit codes:
+# 0 - siker
+# 1 - hiányzó arg
+#
 #!/bin/bash
 echo Hello, $USER!
+
+#cli arg beolvasása + default beállítás
+status=${1:-"Hogy"}
+echo "$status vagy?"
+if [ -z "$1" ]; then
+	echo "Nincs arg."
+	exit 1
+fi
+
+
+#exit code kiírása
+#nem írja ki, ha csak ez van: exit 0
+echo $?
