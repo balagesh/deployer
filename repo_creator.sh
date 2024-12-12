b67b4e051bb008d6ac8f4c4a476e6836fdd091bc
balagesh <balagesh.h@gmail.com>
2024-12-12 13:25:47 +0000

    Repógyártó kész

--- repo_creator.sh
+++ repo_creator.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-name=$USER
-echo "Hi, $USER! What can I help for you?
-May I suggest to create a repository?
-If yes, just type in yes."
+# Exit codes:
+# 0 - siker
+# 1 - hiányzó arg
+# 2 - helytelen elnevezés
+# 3 - nincs jogosultság
 
-read yes
-
-if [ "$yes" = "yes" ]; then
-       echo "Good idea!"
-       echo "Please find below your current branches:"
-
-       git branch -a
-
-       echo -e "\nPlease give me the name of the new repo or type no if you changed your mind!"
-       read repoName
-
-       if [ "$repoName" != "no" ]; then
-		git checkout -b "$repoName"
-		echo "Enjoy!"
-		git branch -a
-	else
-		echo "No worries! Next time then."
-	fi
-else
-	echo "No worries! Next time then."
+# cli arg?
+if [ -z "$1" ]; then
+	echo "Nincs arg."
+	exit 1
 fi
+
+# szabványos elnevezés
+if ! git check-ref-format --allow-onelevel "$1"; then
+	echo "Nem szabványos elnevezés."
+	exit 2
+fi
+
+# jogosultság?
+if [[ ! -w "$PWD" ]]; then
+	echo "Nincs jogosultság."
+	exit 3	
+fi
+
+git checkout -b "$1"
+exit 0
